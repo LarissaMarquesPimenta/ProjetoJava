8f4f5b90977a598e81717d4db2771d449a22f2ac
LarissaMarquesPimenta <laariipimenta@gmail.com>
2023-03-26 18:43:35 -0300

    Primeiro commit - Larissa

---
+++ testePaciente.java
@@ -0,0 +1,65 @@
+package projetoJava;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class testePaciente {
+
+	public static void main(String[] args) { 
+		int opcao; 
+		Scanner leia = new Scanner(System.in); 
+		
+		List<Paciente> pacientes = new ArrayList<>();
+	
+		
+		while(true) {
+			
+			// Menu
+			
+			System.out.println("Escolha uma opção.");
+			opcao = leia.nextInt();
+			
+			switch(opcao) {
+			
+			case 1: 
+				//cadastrar paciente 
+				break;
+				
+			case 2:
+				if (pacientes.isEmpty()){
+					System.out.println("A lista está vazia.");
+					}
+				else {
+					for(Paciente loop:pacientes) {
+						loop.visualizar(); }
+				}	
+				break;
+			
+			case 3: 
+				//Atualizar cadastro
+				break;
+				
+			case 4: 
+				//Excluir dados
+				break;
+				
+			case 5: 
+				//Excluir cadastro
+				break;
+				
+			case 6: 
+				//Sair
+				break;
+				
+			}
+			
+			
+			
+		}
+		
+		
+
+	}
+
+}
