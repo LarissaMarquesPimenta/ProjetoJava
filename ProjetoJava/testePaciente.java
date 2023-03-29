d0bccf47d75209835bfdd8f70040978a0f4c9e4a
LarissaMarquesPimenta <laariipimenta@gmail.com>
2023-03-29 14:17:27 -0300

    exclusão e cadastro atualizado

--- ProjetoJava/testePaciente.java
+++ ProjetoJava/testePaciente.java
@@ -23,8 +23,36 @@ public class testePaciente {
 			
 			switch(opcao) {
 			
-			case 1: 
-				//cadastrar paciente 
+			case 1:
+				boolean loop = true;
+				int op;
+				while(loop == true) {
+					System.out.println("Para continuar escreva o nome do paciente ou sair para finalizar o programa.");
+					String nome = leia.next();
+					System.out.println("Qual tipo sanguineo?");
+					String tipoSanguineo = leia.next();
+					System.out.println("Qual o sexo?");
+					String genero = leia.next();
+					System.out.println("Qual a idade do paciente?");
+					int idade = leia.nextInt();
+					System.out.println("Qual o peso do paciente?");
+					double peso = leia.nextDouble();
+					System.out.println("Qual a altura do paciente?");
+					double altura = leia.nextDouble();
+					pacientes.add(new Paciente(nome, tipoSanguineo, genero, idade, peso, altura));
+					System.out.println("Deseja adicionar outro paciente? ");
+					System.out.println("[1] Sim \n[2] Não");
+					op = leia.nextInt();
+					if (op == 1) {
+						loop = true;
+					}
+					else if (op == 2) {
+						loop = false;
+					}
+					else {
+						System.out.println("Opção invalida! Digite novamente.");
+						op = leia.nextInt();						
+					} }
 				break;
 				
 			case 2: //larissa 
@@ -33,9 +61,9 @@ public class testePaciente {
 					System.out.println("A lista está vazia.");
 					}
 				else {
-					for(Paciente loop:pacientes) {
-						System.out.println((pacientes.indexOf(loop) + 1) + "° paciente");
-						loop.visualizar(); }
+					for(Paciente i:pacientes) {
+						System.out.println((pacientes.indexOf(i) + 1) + "° paciente");
+						i.visualizar(); }
 				}	
 				break;
 			
@@ -44,7 +72,17 @@ public class testePaciente {
 				break;
 				
 			case 4: 
-				//Excluir dados
+				leia.nextLine();
+				System.out.println("\nDigite o paciente que deseja remover: ");
+				int index = leia.nextInt();
+				if(index < pacientes.size()) {
+					pacientes.remove(index);	
+				}
+				else if (pacientes.size() < index) {
+					System.out.println("\nEste paciente não existe!");
+				}
+				System.out.println("\nPacientes no cadrastro: ");
+				System.out.println(index);
 				break;
 				
 			case 5: 
